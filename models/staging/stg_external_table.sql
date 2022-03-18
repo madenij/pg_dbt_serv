e697eab04fa143a3ae2167f3498546f23ee98d19
Mujeeb Adeniji <adenijimujeeb@gmail.com>
2022-03-18 12:54:00 +0000

    1

--- models/staging/stg_external_table.sql
+++ models/staging/stg_external_table.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='view') }}
 
-select * from trips_data_all.external_table
--- from {{ source('staging','external_table') }}
+select *
+from {{ source('staging','external_table') }}
 limit 100
